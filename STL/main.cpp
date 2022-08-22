474ee49877d4adf6c4b42a5038f0c167d7c52e2f
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-08-22 21:53:37 +0300

    Add project files.

---
+++ STL/main.cpp
@@ -0,0 +1,13 @@
+//STL - Standard Template Library
+//Container - это объект, который организ. хранение других объектов в памяти
+//Sequence containers: array, vector, dequeue, list, forward_list
+//Associative cont: map, set, multimap, multriset
+//Adaptor cont: stack, queue, priority_queue
+//Unordered Associative Cont: Unordered_map, Unordered_set, Unordered_multimap/set
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+}
