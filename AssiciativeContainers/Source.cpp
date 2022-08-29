810eddf6dfe430cd71c668d891d84629beca9f84
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-08-29 21:50:32 +0300

    DONE

--- AssiciativeContainers/Source.cpp
+++ AssiciativeContainers/Source.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-//#include<string>
-//#include<set>
-//#include<map>
-//#include<list>
+﻿#include<iostream>
+#include<string>
+#include<set>
+#include<map>
+#include<list>
 //using namespace std;
 using std::cin;
 using std::cout;
@@ -13,8 +13,97 @@ using std::endl;
 //#define STL_SET
 //#define STL_MAP
 
+//class Kitten
+//{
+//	std::string name;
+//public:
+//	Kitten(const std::string&name):name(name){}
+//	bool operator<(const Kitten& other)const
+//	{
+//		return this->name < other.name;
+//	}
+//	bool operator>(const Kitten& other)const
+//	{
+//		return this->name < other.name;
+//	}
+//	friend std::ostream& operator<<(std::ostream&os, const Kitten& obj)
+//	{
+//		return os << obj.name;
+//	}
+//};
+
 void main()
 {
 	setlocale(LC_ALL, "");
+	//                                 key - value
+    //map - каждый элемент map является парой: key - value
+	//set - в каждом эл. контейнера ключ и значение совмещены, 
+	//т.е., ключ и является значением.
 
+	//set - каждый ключ (каждое значение) в set является уникальным, 
+	//т.е., set НЕ может хранить два и более одинаковых значений 
+	//multiset - НЕ уникальное дерево, т.е., значения в нём могут повторяться.
+
+	//map - уникальное дерево, в котором НЕ может быть двух одинаковых ключей(key),
+	//но, с разными ключами могут встречаться одинаковые значения.
+	//multimap - НЕ уникальное дерево, т.е., один и тот же ключ может встреч. более одного раза
+#ifdef STL_SET
+	std::multiset<int>i_set = { 50,25,75,16,16,16,16,32,64,88,88,88,22 };
+	for (int i : i_set)cout << i << tab; cout << endl;
+	for (std::set<int>::reverse_iterator it = i_set.rbegin(); it != i_set.rend(); ++it)
+		cout << *it << tab;
+	cout << endl;
+	//ПРИМЕЧАНИЕ!!!! в ассоциатив. контейнеры можно помещать только значения для которых
+	//перегружены операторы сравнения
+	std::set<std::string>s_set = { "хорошо", "живёт", "на", "свете", "Винни-Пух" };
+	for (std::string i : s_set)cout << i << tab; cout << endl;
+	std::set<Kitten>k_set = { Kitten("Tom"), Kitten("Garfield"), Kitten("Tiger") };
+	for (Kitten i : k_set)cout << i << tab; cout << endl;
+#endif //STL_SET
+#ifdef STL_MAP
+	std::map<int, std::string> week =
+	{
+		std::pair<int, std::string>(0, "Sunday"),
+		std::pair<int, std::string>(1, "Monday"),
+		{2, "Tuesday"},
+		{3, "Wednesday"},
+		{4, "Thursday"},
+		{5, "Friday"},
+		{6, "Saturday"},
+		{7, "Sunday"},
+		{0, "Sunday"},
+		{0, "Sunday"}
+		
+	};
+	for (std::map<int, std::string>::iterator it = week.begin(); it != week.end(); ++it)
+	{
+		cout << it->first << tab << it->second << endl;
+	}
+#endif //STL_MAP
+
+	std::map<std::string, std::list<std::string>>dictionary=
+	{
+		std::pair<std::string, std::list<std::string>>("clutter",{"беспорядок", "хаос"}),
+		{"space", {"пространство","пробел", "космос"}},
+		{"plural", {"множественное число"}},
+		{"worth", {"ценность", "значение", "значимость"}}
+	};
+	/*for (std::map<std::string, std::list<std::string>>::iterator it = dictionary.begin(); it != dictionary.end(); ++it)
+	{
+		cout << it->first << ":\t";
+		for (std::list<std::string>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+		{
+			cout << *it2 << ",";
+		}
+		cout << ";\n";
+	}*/
+	for (std::pair<std::string, std::list<std::string>>i : dictionary)
+	{
+		cout << i.first << ":\t";
+		for (std::string j : i.second)
+		{
+			cout << j << ",";
+		}
+		cout << ";\n";
+	}
 }
